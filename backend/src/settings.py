ddc66fa66c3bd8c1c84c18cb6d89eb4a28139f1b
Dawid <rygula.dawid@gmail.com>
2023-12-19 14:30:55 +0100

    [mg-2] Read SECRET_KEY from env file

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(y#o&s8=1pd73tj1n9pu$)5b$b!j^lemtqtdh&_5vpvh9g)d9n"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+ENVIRONMENT = os.environ.get("APP_ENVIRONMENT")
 
 ALLOWED_HOSTS = ["0.0.0.0", "localhost"]
 
