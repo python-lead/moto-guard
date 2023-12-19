16740f58fa4f486e2941d2a091c4e4bf1cd759cc
Dawid <rygula.dawid@gmail.com>
2023-12-19 14:07:35 +0100

    [mg-2] Import sort backend src

--- backend/src/api_urls.py
+++ backend/src/api_urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.urls import include, path
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
