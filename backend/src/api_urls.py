08a26038a0c68039c006fd7818b38e2be834c7dd
Dawid <rygula.dawid@gmail.com>
2023-12-19 10:48:58 +0100

    [mg-2] Include api_urls in urlpatterns

---
+++ backend/src/api_urls.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+
+urlpatterns = [
+    path("api-auth/", include("rest_framework.urls")),
+    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+]
