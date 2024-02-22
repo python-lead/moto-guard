1e8c396f762b981ccde2cdb3a4b9f0c422bd4aad
Dawid <rygula.dawid@gmail.com>
2024-02-22 10:32:06 +0100

    [mg-5] Add motorcycle factories, implement viewset testcases

---
+++ backend/src/apps/motorcycle/tests/factories.py
@@ -0,0 +1,29 @@
+import factory
+from src.apps.motorcycle.models import Motorcycle, MotorcycleBrand
+
+
+class MotorcycleBrandFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = MotorcycleBrand
+
+    name = factory.Faker("company")
+    country_of_origin = factory.Faker("country")
+    year_established = factory.Faker("year")
+
+
+class MotorcycleFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Motorcycle
+
+    brand = factory.SubFactory(MotorcycleBrandFactory)
+    displacement = factory.Faker("random_int", min=100, max=2000)
+    drive_type = factory.Faker(
+        "random_element", elements=Motorcycle.DriveTypeChoices.choices
+    )
+    model = factory.Faker("word")
+    short_description = factory.Faker("sentence")
+    type = factory.Faker(
+        "random_element",
+        elements=Motorcycle.Type.choices,
+    )
+    year = factory.Faker("random_int", min=1980, max=2024)
---
+++ backend/src/apps/motorcycle/tests/test_motorcycle_viewset.py
@@ -0,0 +1,29 @@
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+from src.apps.motorcycle.tests.factories import (
+    MotorcycleBrandFactory,
+    MotorcycleFactory,
+)
+
+
+class MotorcycleAPITestCase(APITestCase):
+    def _setup_fixtures(self):
+        self.brand = MotorcycleBrandFactory()
+        self.motorcycle = MotorcycleFactory(brand=self.brand)
+
+    def setUp(self):
+        self._setup_fixtures()
+
+        self.url_list = reverse("motorcycle:models-list", args=(self.brand.pk,))
+        self.url_detail = reverse(
+            "motorcycle:models-detail", args=(self.brand.pk, self.motorcycle.pk)
+        )
+
+    def test_list_motorcycles(self):
+        response = self.client.get(self.url_list)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_retrieve_motorcycle(self):
+        response = self.client.get(self.url_detail)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
