710eb543ad4c7481da253777d87d7e0cb878ca2d
Dawid <rygula.dawid@gmail.com>
2024-02-22 10:30:44 +0100

    [mg-5] Add basic motorcycle viewsets

---
+++ backend/src/apps/motorcycle/urls.py
@@ -0,0 +1,14 @@
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
+from src.apps.motorcycle.views import MotorcycleBrandViewSet, MotorcycleViewSet
+
+app_name = "motorcycle"
+
+router = DefaultRouter()
+router.register("brands", MotorcycleBrandViewSet, basename="brands")
+
+brand_router = routers.NestedDefaultRouter(router, "brands", lookup="brand")
+
+brand_router.register("motorcycles", MotorcycleViewSet, basename="models")
+
+urlpatterns = router.urls + brand_router.urls
---
+++ backend/src/apps/motorcycle/views.py
@@ -0,0 +1,16 @@
+from rest_framework import viewsets
+from src.apps.motorcycle.models import Motorcycle, MotorcycleBrand
+from src.apps.motorcycle.serializers import (
+    MotorcycleBrandSerializer,
+    MotorcycleSerializer,
+)
+
+
+class MotorcycleBrandViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = MotorcycleBrand.objects.all()
+    serializer_class = MotorcycleBrandSerializer
+
+
+class MotorcycleViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Motorcycle.objects.all()
+    serializer_class = MotorcycleSerializer
